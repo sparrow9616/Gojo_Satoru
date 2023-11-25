898ae8e992436535a8db45524ee65787391f1d59
Saykkuno (Ashutosh) <130449575+aashuxxD@users.noreply.github.com>
2023-11-24 16:53:06 -0800

    Update start_utils.py

--- Powers/utils/start_utils.py
+++ Powers/utils/start_utils.py
@@ -257,8 +257,8 @@ async def get_help_msg(m: Message or CallbackQuery, help_option: str):
         else:
             mes = m
         help_msg = f"""
-Hey **[{mes.from_user.first_name}](http://t.me/{mes.from_user.username})**
-๏ I'm **[{mes.from_user.BOT_USERNAME}](http://t.me/{mes.from_user.username})** ✨.
+✨ Hey **[{mes.from_user.first_name}](http://t.me/{mes.from_user.username})** 🥀
+๏ I'm **[˹ᴋᴜɴᴏ ✘ ʀᴏʙᴏᴛ˼](https://t.me/KunoXrobot)** !.
 I'm here to help you manage your groups!
 Commands available:
 × /start: Start the bot

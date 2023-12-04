cf939cf1641cc5630b9d8404210701d69c6c031a
Saykkuno (Ashutosh) <130449575+aashuxxD@users.noreply.github.com>
2023-12-04 00:27:14 -0800

    Update quotly.py

--- Powers/plugins/quotly.py
+++ Powers/plugins/quotly.py
@@ -1,93 +1,118 @@
 import random
 from datetime import datetime
 
-import pytz
-from pyrogram import enums, filters
+from io import BytesIO
+from traceback import format_exc
+from pyrogram import filters
+from pyrogram.types import Message
 
-from Powers import Gojo
-from Powers.core.decorators.errors import capture_err
+from wbb import SUDOERS, Gojo
+from wbb.core.decorators.errors import capture_err
 from Powers.utils.dbfunctions import get_couple, save_couple
 
-__MODULE__ = "Shippering"
-__HELP__ = "/detect_gay - To Choose Couple Of The Day"
+__MODULE__ = "Quotly"
+__HELP__ = """
+/q - To quote a message.
+/q [INTEGER] - To quote more than 1 messages.
+/q r - to quote a message with it's reply
 
 
-# Date and time
-def dt():
-    # Set the timezone to Indian Standard Time
-    ist_timezone = pytz.timezone("Asia/Kolkata")
 
-    # Get the current time in IST
-    ist_now = datetime.now(ist_timezone)
-
-    dt_string = ist_now.strftime("%d/%m/%Y %H:%M")
-    dt_list = dt_string.split(" ")
-    return dt_list
+async def quotify(messages: list):
+    response = await arq.quotly(messages)
+    if not response.ok:
+        return [False, response.result]
+    sticker = response.result
+    sticker = BytesIO(sticker)
+    sticker.name = "sticker.webp"
+    return [True, sticker]
 
 
-def dt_tom():
-    a = (
-        str(int(dt()[0].split("/")[0]) + 1)
-        + "/"
-        + dt()[0].split("/")[1]
-        + "/"
-        + dt()[0].split("/")[2]
-    )
-    return a
+def getArg(message: Message) -> str:
+    arg = message.text.strip().split(None, 1)[1].strip()
+    return arg
 
 
-def today():
-    return str(dt()[0])
-
-
-def tomorrow():
-    return str(dt_tom())
-
-
-@Gojo.on_message(filters.command("detect_gay"))
-@capture_err
-async def couple(_, message):
-    if message.chat.type == enums.ChatType.PRIVATE:
-        return await message.reply_text("This command only works in groups.")
-
-    m = await message.reply("Detecting gay among us...")
-
+def isArgInt(message: Message) -> list:
+    count = getArg(message)
     try:
-        chat_id = message.chat.id
-        is_selected = await get_couple(chat_id, today())
-        if not is_selected:
-            list_of_users = []
-            async for i in app.get_chat_members(message.chat.id):
-                if not i.user.is_bot and not i.user.is_deleted:
-                    user = await app.get_users(i.user.id)
-                    list_of_users.append(user.id)
-            if len(list_of_users) < 2:
-                return await m.edit("Not enough users")
-            c1_id = random.choice(list_of_users)
-            c2_id = random.choice(list_of_users)
-            while c1_id == c2_id:
-                c1_id = random.choice(list_of_users)
-            c1_mention = (await app.get_users(c1_id)).mention
-            c2_mention = (await app.get_users(c2_id)).mention
+        count = int(count)
+        return [True, count]
+    except ValueError:
+        return [False, 0]
 
-            couple_selection_message = f"""**Couple of the day:**
-{c1_mention} + {c2_mention} = ❤️
 
-__New couple of the day may be chosen at 12AM {tomorrow()}__"""
-            await m.edit(couple_selection_message)
-            couple = {"c1_id": c1_id, "c2_id": c2_id}
-            await save_couple(chat_id, today(), couple)
+@Gojo.on_message(filters.command("q", prefixes=USERBOT_PREFIX) & SUDOERS)
+@Gojo.on_message(filters.command("q") & ~filters.private & ~filters.edited)
+@capture_err
+async def quotly_func(client, message: Message):
+    if not message.reply_to_message:
+        return await message.reply_text("Reply to a message to quote it.")
+    if not message.reply_to_message.text:
+        return await message.reply_text(
+            "Replied message has no text, can't quote it."
+        )
+    m = await message.reply_text("Quoting Messages")
+    if len(message.command) < 2:
+        messages = [message.reply_to_message]
 
-        elif is_selected:
-            c1_id = int(is_selected["c1_id"])
-            c2_id = int(is_selected["c2_id"])
-            c1_name = (await app.get_users(c1_id)).first_name
-            c2_name = (await app.get_users(c2_id)).first_name
-            couple_selection_message = f"""Couple of the day:
-[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❤️
+    elif len(message.command) == 2:
+        arg = isArgInt(message)
+        if arg[0]:
+            if arg[1] < 2 or arg[1] > 10:
+                return await m.edit("Argument must be between 2-10.")
 
-__New couple of the day may be chosen at 12AM {tomorrow()}__"""
-            await m.edit(couple_selection_message)
+            count = arg[1]
+
+            # Fetching 5 extra messages so tha twe can ignore media
+            # messages and still end up with correct offset
+            messages = [
+                i
+                for i in await client.get_messages(
+                    message.chat.id,
+                    range(
+                        message.reply_to_message.message_id,
+                        message.reply_to_message.message_id + (count + 5),
+                    ),
+                    replies=0,
+                )
+                if not i.empty and not i.media
+            ]
+            messages = messages[:count]
+        else:
+            if getArg(message) != "r":
+                return await m.edit(
+                    "Incorrect Argument, Pass **'r'** or **'INT'**, **EX:** __/q 2__"
+                )
+            reply_message = await client.get_messages(
+                message.chat.id,
+                message.reply_to_message.message_id,
+                replies=1,
+            )
+            messages = [reply_message]
+    else:
+        return await m.edit(
+            "Incorrect argument, check quotly module in help section."
+        )
+    try:
+        if not message:
+            return await m.edit("Something went wrong.")
+
+        sticker = await quotify(messages)
+        if not sticker[0]:
+            await message.reply_text(sticker[1])
+            return await m.delete()
+        sticker = sticker[1]
+        await message.reply_sticker(sticker)
+        await m.delete()
+        sticker.close()
     except Exception as e:
+        await m.edit(
+            "Something went wrong while quoting messages,"
+            + " This error usually happens when there's a "
+            + " message containing something other than text,"
+            + " or one of the messages in-between are deleted."
+        )
+        e = format_exc()
         print(e)
-        await message.reply_text(e)
+        
